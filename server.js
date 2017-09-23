205c13cebbe7a677cb49d97271776ebc6a66c639
tejas22 <31151284+tejas22@users.noreply.github.com>
2017-09-23 16:58:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/article-one', function (req, res) {
+  res.send('this is article1');
+});
+app.get('/article-two', function (req, res) {
+  res.send('this is article2');
+});
+app.get('/article-three', function (req, res) {
+  res.send('this is article3');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
